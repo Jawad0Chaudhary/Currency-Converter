532f82c8c805dd9030e9a1d2cd23a0b41e8e0a1e
Jawad0Chaudhary <jawadchaudhary00@gmail.com>
2024-04-07 02:47:58 +0500

    Added more currency

--- main.js
+++ main.js
@@ -6,21 +6,27 @@ const currency = {
     USD: 1, //Base Currency
     EUR: 0.92,
     GBP: 0.79,
-    INR: 83.30,
+    INR: 83.3,
     PKR: 277.54,
+    KWD: 0.31,
+    BHD: 0.38,
+    OMR: 0.38,
+    JPY: 151.61,
+    BDT: 109.60,
+    CNY: 7.23,
 };
 let userAnswer = await inquirer.prompt([
     {
         name: "from",
         message: chalk.yellow("Enter From Currency"),
         type: "list",
-        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "KWD", "BHD", "OMR", "JPY", "BDT", "CNY"],
     },
     {
         name: "to",
         message: chalk.yellow("Enter To Currency"),
         type: "list",
-        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "KWD", "BHD", "OMR", "JPY", "BDT", "CNY"],
     },
     {
         name: "amount",
@@ -35,4 +41,4 @@ let amount = userAnswer.amount;
 //CALCULATING OR CONVERTING AMOUNT TO DESIRED CURRENCY
 let baseAmount = amount / fromAmount; //USD base currency / from amount
 let convertedAmount = Math.floor(baseAmount * toAmount); //Usd base currency Multiply to amount
-console.log((chalk.blueBright("Coverted Amount Is:")) + (chalk.green(convertedAmount)));
+console.log(chalk.blueBright("Coverted Amount Is:") + chalk.green(convertedAmount));
--- main.ts
+++ main.ts
@@ -3,37 +3,45 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-console.log(chalk.bgWhiteBright.underline.bold.italic.redBright("WELCOME! TO SIMPLE CURRENCY COVERTER"));
+console.log(
+  chalk.bgWhiteBright.underline.bold.italic.redBright(
+    "WELCOME! TO SIMPLE CURRENCY COVERTER"
+  )
+);
 
-const currency :any = {
+const currency: any = {
   USD: 1, //Base Currency
   EUR: 0.92,
   GBP: 0.79,
-  INR: 83.30,
+  INR: 83.3,
   PKR: 277.54,
+  KWD: 0.31,
+  BHD: 0.38,
+  OMR: 0.38,
+  JPY: 151.61,
+  BDT: 109.60,
+  CNY: 7.23,
 };
 
-let userAnswer = await inquirer.prompt(
-  [
-    {
-      name: "from",
-      message: chalk.yellow("Enter From Currency"),
-      type:"list",
-      choices: ["USD", "EUR", "GBP", "INR", "PKR"],
-    },
-    {
-      name: "to",
-      message: chalk.yellow("Enter To Currency"),
-      type: "list",
-      choices: ["USD", "EUR", "GBP", "INR", "PKR"],
-    },
-    {
-      name: "amount",
-      message: "Enter Your Amount:",
-      type: "number",
-    },
-  ]
-);
+let userAnswer = await inquirer.prompt([
+  {
+    name: "from",
+    message: chalk.yellow("Enter From Currency"),
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR", "KWD", "BHD", "OMR", "JPY", "BDT", "CNY"],
+  },
+  {
+    name: "to",
+    message: chalk.yellow("Enter To Currency"),
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR", "KWD", "BHD", "OMR", "JPY", "BDT", "CNY"],
+  },
+  {
+    name: "amount",
+    message: "Enter Your Amount:",
+    type: "number",
+  },
+]);
 
 //  CREATING A VARIABLE
 let fromAmount = currency[userAnswer.from]; //exchange rate
@@ -44,4 +52,6 @@ let amount = userAnswer.amount;
 let baseAmount = amount / fromAmount; //USD base currency / from amount
 let convertedAmount = Math.floor(baseAmount * toAmount); //Usd base currency Multiply to amount
 
-console.log((chalk.blueBright("Coverted Amount Is:")) +(chalk.green(convertedAmount)))
+console.log(
+  chalk.blueBright("Coverted Amount Is:") + chalk.green(convertedAmount)
+);
